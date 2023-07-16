8c0ff27870a80f50bf3e9ee41751aee5caf70170
thesaucemachine <skyler.saucedo@gmail.com>
2023-07-16 20:31:42 +0000

    dont need inference method

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ from PIL import Image
 
 from flask import Flask, render_template, request, redirect
 
-from inference import get_prediction
-from commons import format_class_name
+#from inference import get_prediction
+#from commons import format_class_name
 
 app = Flask(__name__)
 
