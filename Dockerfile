9a8478893b4fb5375321d638ba27285b968bf050
thesaucemachine <skyler.saucedo@gmail.com>
2023-07-19 22:12:25 +0000

    added Matthias's route

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,22 @@
-#FROM tiangolo/uvicorn-gunicorn-fastapi:python3.9
-#WORKDIR /code
-#COPY . /code
-#RUN pip install --upgrade pip &&\
-#    pip install --no-cache-dir --upgrade -r /code/requirements.txt
-#CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8080"]
+FROM tiangolo/uvicorn-gunicorn-fastapi:python3.9
+WORKDIR /code
+COPY . /code
+RUN pip install --upgrade pip &&\
+   pip install --no-cache-dir --upgrade -r /code/requirements.txt
+CMD ["uvicorn", "app:app", "--host", "0.0.0.0", "--port", "8080"]
 
-FROM ubuntu:18.04
-ENV LC_ALL=C
-RUN apt-get update -y && apt-get install -y python3-pip python3-dev build-essential pkg-config
+# FROM ubuntu:18.04
+# ENV LC_ALL=C
+# RUN apt-get update -y && apt-get install -y python3-pip python3-dev build-essential pkg-config
 
-# Install extras
-#COPY requirements.yml /requirements.yml
-COPY requirements.txt /requirements.txt
-COPY . /code/
-# If you are using a py27 image, change this to py27
-#RUN /bin/bash -c ". activate py36 && conda env update -f=/requirements.yml"
-CMD ["bash"]
-RUN pip3 install -r /requirements.txt
-RUN python app.py
+# # Install extras
+# #COPY requirements.yml /requirements.yml
+# COPY requirements.txt /requirements.txt
+# COPY . /code/
+# # If you are using a py27 image, change this to py27
+# #RUN /bin/bash -c ". activate py36 && conda env update -f=/requirements.yml"
+# CMD ["bash"]
+# RUN pip3 install -r /requirements.txt
+# RUN python app.py
 
-#CMD [ "flask", "run", "--host=0.0.0.0" ]
+# #CMD [ "flask", "run", "--host=0.0.0.0" ]
--- app.py
+++ app.py
@@ -2,49 +2,63 @@
 Build a PyTorch model that can be used for prediction served out via FastAPI
 """
 
+# import io
+# import json
+# import torch
+# from torchvision import models
+# import torchvision.transforms as transforms
+# from PIL import Image
+
+# #import fastapi
+# #from fastapi import File, UploadFile
+# #import uvicorn
+
+
+# # updating with Flask
+# from crypt import methods
+# from flask import Flask,jsonify,request,redirect
+
+# #from flask import Flask, render_template, request, redirect
+# from flask_swagger_ui import get_swaggerui_blueprint
+
+# # dont need inference
+# #from inference import get_prediction
+# #from commons import format_class_name
+
+# app = Flask(__name__)
+
+# SWAGGER_URL="/swagger"
+# API_URL="swagger.json"
+
+# # look for cuda device, not on image...
+
+# print('is_available: ', torch.cuda.is_available())
+
+# swagger_ui_blueprint = get_swaggerui_blueprint(
+#     SWAGGER_URL,
+#     API_URL,
+#     config={
+#         'app_name': 'Access API'
+#     }
+# )
+
+# app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
+
+# #app = fastapi.FastAPI()
+
 import io
 import json
-import torch
+
 from torchvision import models
 import torchvision.transforms as transforms
 from PIL import Image
+import fastapi
+from fastapi import File, UploadFile
+import uvicorn
+import torch
+from flask import Flask, request, redirect
 
-#import fastapi
-#from fastapi import File, UploadFile
-#import uvicorn
-
-
-# updating with Flask
-from crypt import methods
-from flask import Flask,jsonify,request,redirect
-
-#from flask import Flask, render_template, request, redirect
-from flask_swagger_ui import get_swaggerui_blueprint
-
-# dont need inference
-#from inference import get_prediction
-#from commons import format_class_name
-
-app = Flask(__name__)
-
-SWAGGER_URL="/swagger"
-API_URL="swagger.json"
-
-# look for cuda device, not on image...
-
-print('is_available: ', torch.cuda.is_available())
-
-swagger_ui_blueprint = get_swaggerui_blueprint(
-    SWAGGER_URL,
-    API_URL,
-    config={
-        'app_name': 'Access API'
-    }
-)
-
-app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
-
-#app = fastapi.FastAPI()
+app = fastapi.FastAPI()
 
 model_10 = torch.load("model_10_class_not_jit.pt", map_location=torch.device('cpu'))
 model_3 = torch.load("model_3_class_not_jit.pt", map_location=torch.device('cpu'))
@@ -52,7 +66,6 @@ model_3 = torch.load("model_3_class_not_jit.pt", map_location=torch.device('cpu'
 model_10.eval()
 model_3.eval()
 
-#imagenet_class_index = json.load(open("imagenet_class_index.json", encoding="utf-8"))
 model10_class_index = json.load(open("10class_index.json", encoding="utf-8"))
 model3_class_index = json.load(open("3class_index.json", encoding="utf-8"))
 
@@ -91,51 +104,78 @@ def get_prediction(image_bytes):
     return model_3_pred_idx, label_pred_3, model_10_pred_idx, label_pred_10
 
 
-# @app.get("/")
-# def index():
-#     return {"message": "Hello Hair Disease July 16 2023"}
-
-# @app.post("/files/")
-# async def create_file(file: bytes = File()):
-#     return {"file_size": len(file)}
-
-# @app.post("/uploadfile/")
-# async def create_upload_file(file: UploadFile):
-#     return {"filename": file.filename}
-
-# @app.post("/predict")
-# async def predict(file: UploadFile = File(...)):
-#     image_bytes = await file.read()
-#     print(len(image_bytes))
-#     model_3_pred_idx, label_pred_3, model_10_pred_idx, label_pred_10 = get_prediction(image_bytes=image_bytes)
-#     return {"earlyorlateID": model_3_pred_idx, "class_name_3": label_pred_3, "diseaseID": model_10_pred_idx, "class_name_10":label_pred_10}
-
-
-# Flask stuff below
-
-@app.route('/')
+@app.get("/")
 def index():
-    return {"message": "FLASK -- Hello Hair Disease July 16 2023"}
+    return {"message": "Hello Hair Disease July 16 2023"}
+
+@app.post("/files/")
+async def create_file(file: bytes = File()):
+    return {"file_size": len(file)}
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
+
+@app.post("/predictOLD")
+async def predict(file: UploadFile = File(...)):
+    image_bytes = await file.read()
+    print(len(image_bytes))
+    model_3_pred_idx, label_pred_3, model_10_pred_idx, label_pred_10 = get_prediction(image_bytes=image_bytes)
+    return {"earlyorlateID": model_3_pred_idx, "class_name_3": label_pred_3, "diseaseID": model_10_pred_idx, "class_name_10":label_pred_10}
+
+# adding Matthias's route
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    if request.method == 'POST':
-        if 'file' not in request.files:
-            return redirect(request.url)
-        file = request.files.get('file')
-        if not file:
-            return
-        img_bytes = file.read()
+  if request.method == 'POST':
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+      data = request.json
+      if not data:
+         return
+      img_bytes = data.get('file')
+    elif (content_type == 'multipart/form-data'):
+      if 'file' not in request.files:
+        return redirect(request.url)
+      file = request.files.get('file')
+      if not file:
+        return
+      img_bytes = file.read()
+    else: 
+      return "Content type is not supported."
+   
+    if img_bytes:   # not sure if that works like that in Python...
+      model_3_pred_idx, label_pred_3, model_10_pred_idx, label_pred_10 = get_prediction(image_bytes=img_bytes)        
+      return {"earlyorlateID": model_3_pred_idx, "class_name_3": label_pred_3, "diseaseID": model_10_pred_idx, "class_name_10":label_pred_10}
+    else: 
+      return "Cannot extract image data from request"
 
-        #class_id, class_name = get_prediction(image_bytes=img_bytes)
-        #class_name = format_class_name(class_name)
-        #return render_template('result.html', class_id=class_id, class_name=class_name)
-        # return render_template('index.html')
 
-        model_3_pred_idx, label_pred_3, model_10_pred_idx, label_pred_10 = get_prediction(image_bytes=img_bytes)
+# # Flask stuff below
+
+# @app.route('/')
+# def index():
+#     return {"message": "FLASK -- Hello Hair Disease July 16 2023"}
+
+# @app.route('/predict', methods=['GET', 'POST'])
+# def predict():
+#     if request.method == 'POST':
+#         if 'file' not in request.files:
+#             return redirect(request.url)
+#         file = request.files.get('file')
+#         if not file:
+#             return
+#         img_bytes = file.read()
+
+#         #class_id, class_name = get_prediction(image_bytes=img_bytes)
+#         #class_name = format_class_name(class_name)
+#         #return render_template('result.html', class_id=class_id, class_name=class_name)
+#         # return render_template('index.html')
+
+#         model_3_pred_idx, label_pred_3, model_10_pred_idx, label_pred_10 = get_prediction(image_bytes=img_bytes)
         
-        return {"earlyorlateID": model_3_pred_idx, "class_name_3": label_pred_3, "diseaseID": model_10_pred_idx, "class_name_10":label_pred_10}
+#         return {"earlyorlateID": model_3_pred_idx, "class_name_3": label_pred_3, "diseaseID": model_10_pred_idx, "class_name_10":label_pred_10}
 
 if __name__ == "__main__":
-    #uvicorn.run(app, host="0.0.0.0", port=8080)
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
+    #app.run(debug=True, host='0.0.0.0', port=8080)
